9d026bdb3c4ae50313a17882bd64e107ac6cb9bc
feli <killerkralle@gmail.com>
2020-07-13 12:54:05 +0200

    3D Track Asset Test

--- RailroadTrader_UnityProject/Assets/Scripts/TimeController.cs
+++ RailroadTrader_UnityProject/Assets/Scripts/TimeController.cs
@@ -4,6 +4,19 @@ using UnityEngine;
 
 public class TimeController : MonoBehaviour
 {
+    [Tooltip("How many minutes in real time is 1 ingame hour")]
+    public float realTimeMinPerHour = 2.0f;
+    [Tooltip("paused, normal, fast, super fast")]
+    public float[] ingameSpeed = new float[4];
+       
+    
+    private int curHour;
+    private int curDay;
+    private int curMonth;
+    private int curYear;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
