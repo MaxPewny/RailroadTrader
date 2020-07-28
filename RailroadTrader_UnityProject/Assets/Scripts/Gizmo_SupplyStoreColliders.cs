926f5c75464b547286736bfe4c1ca453c15ea1b6
feli <killerkralle@gmail.com>
2020-07-28 14:52:28 +0200

    Improved NPC movement and avoidance

--- RailroadTrader_UnityProject/Assets/Scripts/Gizmo_SupplyStoreColliders.cs
+++ RailroadTrader_UnityProject/Assets/Scripts/Gizmo_SupplyStoreColliders.cs
@@ -6,15 +6,15 @@ using UnityEngine.AI;
 
 public class Gizmo_SupplyStoreColliders 
 {
-    [DrawGizmo(GizmoType.NonSelected)]
-    static void DrawNonSelectedBuildings(GizmoShop store, GizmoType gizmoType)
-    {
-        NavMeshObstacle col = store.gameObject.GetComponentInChildren<NavMeshObstacle>();
-        Gizmos.color = Color.magenta;
-        Gizmos.DrawWireCube(col.center+store.transform.position, col.size);
-        Gizmos.color = Color.cyan;
-        //Gizmos.DrawWireSphere(store.transform.position, store.maxR);
-    }
+    //[DrawGizmo(GizmoType.NonSelected)]
+    //static void DrawNonSelectedBuildings(GizmoShop store, GizmoType gizmoType)
+    //{
+    //    NavMeshObstacle col = store.gameObject.GetComponentInChildren<NavMeshObstacle>();
+    //    Gizmos.color = Color.magenta;
+    //    Gizmos.DrawWireCube(col.center+store.transform.position, col.size);
+    //    Gizmos.color = Color.cyan;
+    //    //Gizmos.DrawWireSphere(store.transform.position, store.maxR);
+    //}
 
     [DrawGizmo(GizmoType.Selected)]
     static void DrawSelectedBuildings(GizmoShop store, GizmoType gizmoType)
